412b24115508e131600c7dfd4c5f72dabfbb476a
kesava-projects <kesavaharshavardhan@gmail.com>
2026-05-29 23:19:45 +0530

    auth done

--- drf_project/auth_app/urls.py
+++ drf_project/auth_app/urls.py
@@ -1,8 +1,13 @@
+import profile
+
 from django.urls import path
-from .views import register, login
+from .views import register, login, profile, update_profile, delete_profile
 
 urlpatterns = [
     # path("login", login),
     path("register", register),
-    path("login", login)
+    path("login", login),
+    path("profile", profile),
+    path("update", update_profile),
+    path("delete", delete_profile)
 ]
--- drf_project/auth_app/views.py
+++ drf_project/auth_app/views.py
@@ -1,5 +1,7 @@
 import jwt
+from django.http import JsonResponse
 from django.shortcuts import render
+from jwt import InvalidTokenError, ExpiredSignatureError
 from rest_framework import status
 from rest_framework.response import Response
 from .models import UserModel
@@ -9,8 +11,44 @@ SECRET_KEY = 'django-insecure-iwky+iutr@j%z%huxc%6tejz*sf#+ptm_!5i=t1$v@h&+zn8%0
 from .userserializer import UserSerializer
 from rest_framework.decorators import api_view
 
-
 # Create your views here.
+
+def is_authenticated(func):
+    def wrapper(request):
+        auth_header=request.headers.get("Authorization")
+        if auth_header is None:
+            return Response({
+                "message":"Token is missing"
+            })
+        else:
+            token=auth_header.split(" ")[1]
+            try:
+                decoded_token = jwt.decode(
+                    token,
+                    SECRET_KEY,
+                    algorithms=['HS256']
+                )
+                user = UserModel.objects.filter(email=decoded_token['email']).first()
+                if user is None:
+                    return Response({
+                        "message": "User no longer exists"
+                    })
+                request.user = user
+
+            except jwt.ExpiredSignatureError:
+                return Response({
+                    "message": "Token has expired"
+                }, status=401)
+
+            except jwt.InvalidTokenError:
+                return Response({
+                    "message": "Invalid token"
+                }, status=401)
+
+            return func(request)
+
+    return wrapper
+
 
 @api_view(['POST'])
 def register(request):
@@ -23,7 +61,9 @@ def register(request):
             return Response(user_serializer.errors)
 
     except Exception as e:
-        return Response(user_serializer.errors)
+        return Response({
+            "message": str(e)
+        })
 
 @api_view(['POST'])
 def login(request):
@@ -41,6 +81,32 @@ def login(request):
         return Response({'message': 'User logged in',
                          'token': token})
     return Response({'message': 'Invalid password'})
+
+@api_view(['GET'])
+@is_authenticated
+def profile(request):
+    return Response({
+        "id": request.user.id,
+        "email": request.user.email,
+        "phone": request.user.phone
+    })
+
+@api_view(['PUT'])
+@is_authenticated
+def update_profile(request):
+    request.user.phone = request.data['phone']
+    request.user.save()
+    return Response({'message': 'Profile Updated'})
+
+@api_view(['DELETE'])
+@is_authenticated
+def delete_profile(request):
+    user = UserModel.objects.get(id=request.user.id)
+    user.delete()
+    return Response({'message': 'User Deleted'})
+
+
+
 
 
 
